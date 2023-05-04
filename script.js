b28458ee21d1f8c928e65620627083cd6729c26f
Josh Hebert <jhebertcocktails@gmail.com>
2023-05-04 11:42:45 -0400

    workable jS

--- script.js
+++ script.js
@@ -7,22 +7,42 @@ var characters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+
 var generateBtn = document.querySelector("#generate");
 // Starts function to generate password
 function generatePassword() {
-  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  var enter = (prompt("How many characters would you like your password? Choose between 8 and 128"));
   // First if statement for password 
   if (!enter) {
-      alert("You must enter a number!");
+    alert("You must enter a number!");
   } else if (enter < 8 || enter > 128) {
-      // Validates user input
-      // prompts user to add # between 8-128 if enter is hit with no numbers entered
-      enter = parseInt(prompt("Try again, please choose a # between 8 and 128"));
+    // Validates user input
+    // prompts user to add # between 8-128 if enter is hit with no numbers entered
+    enter = (prompt("Try again, please choose a # between 8 and 128"));
 
   } else {
-      // Continues once user input is validated
-      confirmNumber = confirm("Do you want numbers in Password?");
-      confirmCharacter = confirm("Do you want special characters in Password?");
-      confirmUppercase = confirm("Do you want Uppercase letters?");
-      confirmLowercase = confirm("Do you want Lowercase letters?");
+    // Continues once user input is validated
+    var confirmNumber = confirm("Do you want numbers in Password?");
+    var confirmCharacter = confirm("Do you want special characters in Password?");
+    var confirmUppercase = confirm("Do you want Uppercase letters?");
+    var confirmLowercase = confirm("Do you want Lowercase letters?");
+    var combo = [];
+    if (confirmNumber) {
+      combo = [...numbers]
+    }
+    if (confirmCharacter) {
+      combo = [...combo, ...characters]
+    }
+    if (confirmUppercase) {
+      combo = [...combo, ...uppercase]
+    }
+    if (confirmLowercase) {
+      combo = [ ...combo, ...lowercase]
+       } 
+    var capture = ""
+    for (var i = 0; i<enter; i++) {
+     var random = combo [Math.floor(Math.random()*combo.length)]
+     capture = random + capture
+    }
+    return capture  
   };
+
 }
 // Write password to the #password input
 function writePassword() {
